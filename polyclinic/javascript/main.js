687f53413a1adfc210913eac14aa55a542a6ec55
TimStt <timoshka200004@gmail.com>
2023-05-23 20:51:46 +0300

    medical-site

--- polyclinic/javascript/main.js
+++ polyclinic/javascript/main.js
@@ -1,5 +1,19 @@
-const btnClick = document.getElementById('btn-hero-area')
-const formLogin = document.getElementById('form-login')
-    btnClick.onclick = function () {
-        formLogin.scrollIntoView({ behavior: "smooth" });
-    };
+
+// при вызове метода страница плавно скролиться вниз к форме отправки
+
+const btnForm = document.getElementById('btn-hero-area');
+const formLogin = document.getElementById('form-login');
+btnForm.onclick = function () {
+    formLogin.scrollIntoView({ behavior: "smooth" });
+};
+
+const btnFormSend = document.getElementById('send');
+const textBoxName = document.getElementById('nameInput');
+const textBoxNumber = document.getElementById('numberInput');
+let valueName = textBoxName.value;
+let valueNumber = textBoxNumber.value;
+btnFormSend.onclick = function(event){
+    if(valueName == ""|| valueNumber.onemptied == "") {
+        alert("введите данные");
+    }
+}
